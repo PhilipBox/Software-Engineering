2575158c0594ca61afa25de5a7f7bdb0b2b7d633
Philip <jihwanla17@naver.com>
2018-05-21 17:59:17 +0900

    SE JUnit

---
+++ 4_JUnit/case2_myCodes/ErrorCase/Calculator.java
@@ -0,0 +1,49 @@
+package test;
+
+public class Calculator {
+	double x,x1,y1;
+	double y,x2,y2;
+	double distance;
+	
+	public double getX() {
+		x= Math.pow(x1-x2, 2);
+		return x;
+	}
+	
+	public double getY() {
+		y = Math.pow(y1-y2, 2);
+		return y;
+	}
+	
+	public void setX(double X1, double X2) {
+		this.x1 = X1;
+		this.x2 = X2;
+	}
+	
+	public void setY(double Y1, double Y2) {
+		this.y1 = Y1;
+		this.y2 = Y2;
+	}
+	
+	public double getDistance() {
+		return Math.sqrt(getX()+getY());
+	}
+	
+	public String toString() {
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 4_JUnit/case2_myCodes/ErrorCase/TestRunner2.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class TestRunner2 {
+	
+	@Test
+	public void equalsError() {
+		Calculator cal = new Calculator();
+		cal.setX(1,4);
+		cal.setY(1,1);
+		assertEquals(1.0, cal.getDistance());
+	}
+	
+	
+	@Test
+	public void ConditionError() {
+		Calculator cal = new Calculator();
+		
+		cal.setX(2,3);
+		cal.setY(2,9);
+		assertTrue(cal.getDistance()<0);
+	}
+	
+	@Test
+	public void NotNullError() {
+		Calculator cal = new Calculator();
+		cal.setX(1,2);
+		cal.setY(1,1);
+		assertNotNull(cal.toString());
+	}
+	
+}
+
+
