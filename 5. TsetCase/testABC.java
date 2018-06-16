df8f75a21184631cdcb1ededa0baee308682ebd9
Philip <jihwanla17@naver.com>
2018-06-16 18:18:06 +0900

    TestCase update

---
+++ 5. TsetCase/testABC.java
@@ -0,0 +1,17 @@
+package example;
+
+public class testABC {
+
+	
+	public double cube(int a, int b) {
+		
+		double cube_result;
+		
+		cube_result = Math.pow(a, b);
+		
+		
+		return cube_result;
+	}
+}
+
+
